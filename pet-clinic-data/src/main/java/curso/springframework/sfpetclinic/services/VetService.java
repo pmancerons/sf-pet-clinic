3934808d11bff088869dde20ae19f8707c1d6e80
Pedro <pmancerons@gmail.com>
2020-10-25 13:47:36 -0500

    Crud service implementacion an inheritance from there on OwnerService, PerService and VetService

--- pet-clinic-data/src/main/java/curso/springframework/sfpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/curso/springframework/sfpetclinic/services/VetService.java
@@ -1,15 +1,9 @@
 package curso.springframework.sfpetclinic.services;
 
-import curso.springframework.sfpetclinic.model.Owner;
 import curso.springframework.sfpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet,Long>{
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }

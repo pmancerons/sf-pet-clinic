a3281205161431248d58fcabd78d0902817bf8c6
Pedro <pmancerons@gmail.com>
2020-10-25 14:24:04 -0500

    Implementation of map service and inheritance for the entities

---
+++ pet-clinic-data/src/main/java/curso/springframework/sfpetclinic/services/map/PerServiceMap.java
@@ -0,0 +1,33 @@
+package curso.springframework.sfpetclinic.services.map;
+
+import curso.springframework.sfpetclinic.model.Pet;
+import curso.springframework.sfpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PerServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}

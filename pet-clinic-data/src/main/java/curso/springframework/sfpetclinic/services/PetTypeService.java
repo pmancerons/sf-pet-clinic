7e9de539effb47e8bfd8a3e44860bb1c0c975fe7
Pedro <pmancerons@gmail.com>
2020-10-28 16:09:11 -0500

    add Owner contact, create Specialty and create the relation with vet creation of pettype service and its map implementation

---
+++ pet-clinic-data/src/main/java/curso/springframework/sfpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package curso.springframework.sfpetclinic.services;
+
+import curso.springframework.sfpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
